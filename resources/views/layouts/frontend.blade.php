a367a4a99189a280765b235638dc5866587e8eee
mariam7542 <152164756+mariam7542@users.noreply.github.com>
2023-12-07 14:07:22 +0200

    update style.css

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -198,16 +198,12 @@
 	@endif
 
 	<script src="{{asset('public/frontend/js/wow.min.js')}}"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
 
 	<script>
 	
 
               new WOW().init();
 			  
-		 window.addEventListener('scroll', function() {
-            wow.sync();
-        });
 
     </script>
 	
