31b4c909137fa31ffad75fc8270399929cd74d37
progbasma <prog.basma@gmail.com>
2023-12-19 05:06:08 +0200

    footer

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -28,7 +28,7 @@
 		<div class="container">
 			<div class="row">
 
-				<div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 footer-border">
+				<div class="col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3 footer-border">
 					@if($gtext['is_publish_contact'] == 1)
 					<div class="footer-widget-card">
 						<div class="footer-widget mb10">
@@ -79,7 +79,7 @@
 
 				</div>
 
-				<div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+				<div class="col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3">
 					@if(FooterMenuList('footer') != '')
 					<div class="footer-widget-card">
 						<div class="footer-widget mb25">
@@ -88,32 +88,23 @@
 								@php echo FooterMenuList('footer'); @endphp
 							</ul>
 						</div>
-					</div>
-					@endif
-
-				</div>
-				<div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 footer-border">
-					<div class="footer-widget-card">
 						@if($gtext['is_publish_about'] == 1)
 						<div class="footer-widget mb25  payment-method">
-							<div class="info-card">
-								@if($gtext['about_logo_footer'] != '')
-								<div class="info-logo">
-									<img src="{{ asset('public/media/'.$gtext['about_logo_footer']) }}" alt="" />
-								</div>
-								@endif
-								@if($gtext['about_desc_footer'] != '')
-
-								@endif
-							</div>
+							
 						</div>
 						@endif
 
 						<div class="social-media mb25">
 							@php echo SocialMediaList(); @endphp
-                            <iframe style="border-radius:4px" src="https://www.google.com/maps/embed/v1/place?q=https://www.google.com/maps/place/%D9%81%D9%86%D8%AF%D9%82+%D9%85%D9%8A%D8%B1%D8%A7+%D8%A7%D8%B9%D9%85%D8%A7%D9%84+%D8%A7%D9%84%D8%B9%D9%84%D9%8A%D8%A7+MIRA+BUSINESS+HOTEL%E2%80%AD/@24.6886334,46.6880452,17z/data=!4m9!3m8!1s0x3e2ee2a4e0047b25:0x8a10d3a1b417d8ff!5m2!4m1!1i2!8m2!3d24.6886334!4d46.6880452!16s%2Fg%2F11tmr325b4&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
 						</div>
+					</div>
+					@endif
 
+				</div>
+				<div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 footer-border">
+					<div class="footer-widget-card">
+			
+						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14500.45304278439!2d46.688045!3d24.688633!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2ee2a4e0047b25%3A0x8a10d3a1b417d8ff!2z2YHZhtiv2YIg2YXZitix2Kcg2KfYudmF2KfZhCDYp9mE2LnZhNmK2KcgTUlSQSBCVVNJTkVTUyBIT1RFTA!5e0!3m2!1sen!2sus!4v1702953402433!5m2!1sen!2sus" width="100%" height="250" style="border:0;border-radius:4px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 					</div>
 				</div>
 
