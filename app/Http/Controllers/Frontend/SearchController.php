11ad02a9d70ea16124fad79f5e8abf22b13e87e4
Hassan <kerdashhassan@gmail.com>
2023-12-19 15:30:06 +0200

    Update SearchController.php

--- app/Http/Controllers/Frontend/SearchController.php
+++ app/Http/Controllers/Frontend/SearchController.php
@@ -13,13 +13,13 @@ class SearchController extends Controller
 		$lan = glan();
 		$total_adult = $request->total_adult;
 		$total_child = $request->total_child;
-		
+
 		$datalist = DB::table('rooms')
 			->join('categories', 'rooms.cat_id', '=', 'categories.id')
 			->select('rooms.*', 'categories.name as category_name', 'categories.slug as category_slug')
 			->where('rooms.is_publish', '=', 1)
-			->where('rooms.total_adult', '=', $total_adult)
-			->where('rooms.total_child', '=', $total_child)
+			->where('rooms.total_adult', '<=', $total_adult)
+			->where('rooms.total_child', '<=', $total_child)
 			->where('rooms.lan', '=', $lan)
 			->orderBy('rooms.id','desc')
 			->get();
