f4f3d38f5ad66831445cbfe631d5921b5a8f1937
Hassan <kerdashhassan@gmail.com>
2023-12-21 12:23:52 +0200

    Update services.blade.php

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -94,7 +94,7 @@
                     <div>
                         <img style="height:250px" src="{{ asset('public/media/'.$row->image) }}" style="height:200px">
                     </div>
-                    <div class="col-9 bg-white p-4" style="width:100%">
+                    <div class="col-9 bg-white p-4 mb-4" style="width:100%">
                         <h3>{{ $row->title }}</h3>
                         <p class="paragraphe-text" style="height:210px;overflow:auto;">
                             {{ $row->desc }}.
