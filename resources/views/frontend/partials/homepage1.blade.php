2ddd80d29154a2ec8b946778863dba2c5973fdd7
Hassan <kerdashhassan@gmail.com>
2023-12-21 15:23:29 +0200

    Update homepage1.blade.php

--- resources/views/frontend/partials/homepage1.blade.php
+++ resources/views/frontend/partials/homepage1.blade.php
@@ -6,7 +6,8 @@
         <div class="hero-slider">
             @foreach ($slider as $row)
             @php $aRow = json_decode($row->desc); @endphp
-            <div class="hero-screen hero-overlay" style="height:750px; background-image: url({{ $row->image ? asset('public/media/'.$row->image) : '' }});">
+            <div>
+                <div class="hero-screen hero-overlay" style="height:750px; background-image: url({{ asset('public/media/'.$row->image) }});">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
@@ -23,6 +24,7 @@
                     </div>
                 </div>
             </div>
+        </div>
             @endforeach
         </div>
 
