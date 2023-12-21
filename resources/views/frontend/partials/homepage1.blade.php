87467b05f32851795f14a3df8a1a39e076d9a010
Hassan <kerdashhassan@gmail.com>
2023-12-21 14:35:12 +0200

    Update homepage1.blade.php

--- resources/views/frontend/partials/homepage1.blade.php
+++ resources/views/frontend/partials/homepage1.blade.php
@@ -27,7 +27,7 @@
         </div>
 
         <div class="search-card">
-			<div class="col-2 mx-lg-5">
+			<div class="col-3 mx-lg-5">
 				<div class="search-card-inner wow fadeIn">
 
                     <h2 class="text-center">{{ __('Booking Your Hotel') }}</h2>
