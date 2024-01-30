73f945c93a655e6f811e778b82301933d92fcf83
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2024-01-30 14:43:36 +0200

    invoice pdf logo

--- app/Http/Controllers/Frontend/InvoiceController.php
+++ app/Http/Controllers/Frontend/InvoiceController.php
@@ -122,6 +122,15 @@ class InvoiceController extends Controller
 				<td class="w-15" align="right"><p>' . $subtotal . '</p>' . $cal_old_price . '</td>
 			</tr>';
 
+            $item_listar = '<tr>
+            <td class="w-15" align="right"><p>' . $subtotal . '</p>' . $cal_old_price . '</td>
+            <td class="w-10" align="center">' . $total_days . '</td>
+            <td class="w-20" align="center"><p>' . date('d-m-Y', strtotime($mdata['in_date'])) . '</p><p>to</p><p>' . date('d-m-Y', strtotime($mdata['out_date'])) . '</p></td>
+            <td class="w-15" align="center"><p>' . $total_price . '</p>' . $old_price . '</td>
+            <td class="w-10" align="center">' . $mdata['total_room'] . '</td>
+            <td class="w-30" align="left">' . $transTitle[glan()] . '</td>
+        </tr>';
+
         $RoomDataList = DB::table('room_manages')
             ->join('room_assigns', 'room_manages.id', '=', 'room_assigns.room_id')
             ->select('room_manages.room_no')
@@ -146,6 +155,7 @@ class InvoiceController extends Controller
 
         $base_url = url('/');
         $logo = public_path('media/' . $gtext['front_logo']);
+        $logo2= public_path('media/' .'photo_5892981411613359903_x.jpg');
 
         //set font
         PDF::SetFont('dejavusans', '', 9);
@@ -268,7 +278,7 @@ class InvoiceController extends Controller
 		<!--html table-->
 		<table class="border-header" width="100%" cellpadding="10" cellspacing="0">
 			<tr>
-				<td class="w-40"><span class="company-logo"><img src="' . $logo . '"/></span></td>
+				<td class="w-40"><span class="company-logo"><img src="' . $logo2 . '"/></span></td>
 				<td class="w-60 invoice-name">' . __('Invoice') . '</td>
 			</tr>
 		</table>
@@ -583,7 +593,7 @@ class InvoiceController extends Controller
 		</table>
 
 		<table class="border-tb" width="100%" cellpadding="6" cellspacing="0">
-			' . $item_list . '
+			' . $item_listar . '
 		</table>
 
 		<table class="border-none" width="100%" cellpadding="2" cellspacing="0">
@@ -640,176 +650,176 @@ class InvoiceController extends Controller
         PDF::Output('invoice-' . $mdata['booking_no'] . time() . '.pdf', 'D');
     }
 
-    public function getInvoice2($booking_id, $booking_no)
-    {
-        $gtext = gtext();
+//     public function getInvoice2($booking_id, $booking_no)
+//     {
+//         $gtext = gtext();
 
-        $datalist = DB::table('booking_manages')
-            ->join('rooms', 'booking_manages.roomtype_id', '=', 'rooms.id')
-            ->join('payment_method', 'booking_manages.payment_method_id', '=', 'payment_method.id')
-            ->join('payment_status', 'booking_manages.payment_status_id', '=', 'payment_status.id')
-            ->join('booking_status', 'booking_manages.booking_status_id', '=', 'booking_status.id')
-            ->select('booking_manages.*', 'rooms.title', 'rooms.old_price', 'rooms.is_discount',
-                'payment_method.method_name', 'payment_status.pstatus_name', 'booking_status.bstatus_name')
-            ->where('booking_manages.id', $booking_id)
-            ->get();
+//         $datalist = DB::table('booking_manages')
+//             ->join('rooms', 'booking_manages.roomtype_id', '=', 'rooms.id')
+//             ->join('payment_method', 'booking_manages.payment_method_id', '=', 'payment_method.id')
+//             ->join('payment_status', 'booking_manages.payment_status_id', '=', 'payment_status.id')
+//             ->join('booking_status', 'booking_manages.booking_status_id', '=', 'booking_status.id')
+//             ->select('booking_manages.*', 'rooms.title', 'rooms.old_price', 'rooms.is_discount',
+//                 'payment_method.method_name', 'payment_status.pstatus_name', 'booking_status.bstatus_name')
+//             ->where('booking_manages.id', $booking_id)
+//             ->get();
 
-        $mdata = array();
-        foreach ($datalist as $row) {
-            $mdata['title'] = $row->title;
-            $mdata['is_discount'] = $row->is_discount;
-            $mdata['old_price'] = $row->old_price;
-            $mdata['booking_no'] = $row->booking_no;
-            $mdata['payment_status_id'] = $row->payment_status_id;
-            $mdata['booking_status_id'] = $row->booking_status_id;
-            $mdata['total_room'] = $row->total_room;
-            $mdata['total_price'] = $row->total_price;
-            $mdata['discount'] = $row->discount;
-            $mdata['tax'] = $row->tax;
-            $mdata['subtotal'] = $row->subtotal;
-            $mdata['total_amount'] = $row->total_amount;
-            $mdata['in_date'] = $row->in_date;
-            $mdata['out_date'] = $row->out_date;
-            $mdata['customer_name'] = $row->name;
-            $mdata['customer_email'] = $row->email;
-            $mdata['customer_address'] = $row->address;
-            $mdata['city'] = $row->city;
-            $mdata['state'] = $row->state;
-            $mdata['zip_code'] = $row->zip_code;
-            $mdata['country'] = $row->country;
-            $mdata['customer_phone'] = $row->phone;
-            $mdata['created_at'] = $row->created_at;
-            $mdata['method_name'] = $row->method_name;
-            $mdata['pstatus_name'] = $row->pstatus_name;
-            $mdata['bstatus_name'] = $row->bstatus_name;
-        }
+//         $mdata = array();
+//         foreach ($datalist as $row) {
+//             $mdata['title'] = $row->title;
+//             $mdata['is_discount'] = $row->is_discount;
+//             $mdata['old_price'] = $row->old_price;
+//             $mdata['booking_no'] = $row->booking_no;
+//             $mdata['payment_status_id'] = $row->payment_status_id;
+//             $mdata['booking_status_id'] = $row->booking_status_id;
+//             $mdata['total_room'] = $row->total_room;
+//             $mdata['total_price'] = $row->total_price;
+//             $mdata['discount'] = $row->discount;
+//             $mdata['tax'] = $row->tax;
+//             $mdata['subtotal'] = $row->subtotal;
+//             $mdata['total_amount'] = $row->total_amount;
+//             $mdata['in_date'] = $row->in_date;
+//             $mdata['out_date'] = $row->out_date;
+//             $mdata['customer_name'] = $row->name;
+//             $mdata['customer_email'] = $row->email;
+//             $mdata['customer_address'] = $row->address;
+//             $mdata['city'] = $row->city;
+//             $mdata['state'] = $row->state;
+//             $mdata['zip_code'] = $row->zip_code;
+//             $mdata['country'] = $row->country;
+//             $mdata['customer_phone'] = $row->phone;
+//             $mdata['created_at'] = $row->created_at;
+//             $mdata['method_name'] = $row->method_name;
+//             $mdata['pstatus_name'] = $row->pstatus_name;
+//             $mdata['bstatus_name'] = $row->bstatus_name;
+//         }
 
-        $total_days = DateDiffInDays($mdata['in_date'], $mdata['out_date']);
+//         $total_days = DateDiffInDays($mdata['in_date'], $mdata['out_date']);
 
-        $totalPrice = 0;
-        if ($mdata['total_price'] != '') {
-            $totalPrice = $mdata['total_price'];
-        }
+//         $totalPrice = 0;
+//         if ($mdata['total_price'] != '') {
+//             $totalPrice = $mdata['total_price'];
+//         }
 
-        $oldPrice = 0;
-        if ($mdata['old_price'] != '') {
-            $oldPrice = $mdata['old_price'];
-        }
+//         $oldPrice = 0;
+//         if ($mdata['old_price'] != '') {
+//             $oldPrice = $mdata['old_price'];
+//         }
 
-        $sub_total = 0;
-        if ($mdata['subtotal'] != '') {
-            $sub_total = $mdata['subtotal'];
-        }
+//         $sub_total = 0;
+//         if ($mdata['subtotal'] != '') {
+//             $sub_total = $mdata['subtotal'];
+//         }
 
-        $totalTax = 0;
-        if ($mdata['tax'] != '') {
-            $totalTax = $mdata['tax'];
-        }
+//         $totalTax = 0;
+//         if ($mdata['tax'] != '') {
+//             $totalTax = $mdata['tax'];
+//         }
 
-        $totalDiscount = 0;
-        if ($mdata['discount'] != '') {
-            $totalDiscount = $mdata['discount'];
-        }
+//         $totalDiscount = 0;
+//         if ($mdata['discount'] != '') {
+//             $totalDiscount = $mdata['discount'];
+//         }
 
-        $totalAmount = 0;
-        if ($mdata['total_amount'] != '') {
-            $totalAmount = $mdata['total_amount'];
-        }
+//         $totalAmount = 0;
+//         if ($mdata['total_amount'] != '') {
+//             $totalAmount = $mdata['total_amount'];
+//         }
 
-        $calOldPrice = $oldPrice * $mdata['total_room'] * $total_days;
+//         $calOldPrice = $oldPrice * $mdata['total_room'] * $total_days;
 
-        if ($gtext['currency_position'] == 'left') {
-            $oPrice = $gtext['currency_icon'] . NumberFormat($oldPrice);
-            $caloPrice = $gtext['currency_icon'] . NumberFormat($calOldPrice);
-            $total_price = $gtext['currency_icon'] . NumberFormat($totalPrice);
-            $subtotal = $gtext['currency_icon'] . NumberFormat($sub_total);
-            $tax = $gtext['currency_icon'] . NumberFormat($totalTax);
-            $discount = $gtext['currency_icon'] . NumberFormat($totalDiscount);
-            $total_amount = $gtext['currency_icon'] . NumberFormat($totalAmount);
+//         if ($gtext['currency_position'] == 'left') {
+//             $oPrice = $gtext['currency_icon'] . NumberFormat($oldPrice);
+//             $caloPrice = $gtext['currency_icon'] . NumberFormat($calOldPrice);
+//             $total_price = $gtext['currency_icon'] . NumberFormat($totalPrice);
+//             $subtotal = $gtext['currency_icon'] . NumberFormat($sub_total);
+//             $tax = $gtext['currency_icon'] . NumberFormat($totalTax);
+//             $discount = $gtext['currency_icon'] . NumberFormat($totalDiscount);
+//             $total_amount = $gtext['currency_icon'] . NumberFormat($totalAmount);
 
-        } else {
-            $oPrice = NumberFormat($oldPrice) . $gtext['currency_icon'];
-            $caloPrice = NumberFormat($calOldPrice) . $gtext['currency_icon'];
-            $total_price = NumberFormat($totalPrice) . $gtext['currency_icon'];
-            $subtotal = NumberFormat($sub_total) . $gtext['currency_icon'];
-            $tax = NumberFormat($totalTax) . $gtext['currency_icon'];
-            $discount = NumberFormat($totalDiscount) . $gtext['currency_icon'];
-            $total_amount = NumberFormat($totalAmount) . $gtext['currency_icon'];
-        }
+//         } else {
+//             $oPrice = NumberFormat($oldPrice) . $gtext['currency_icon'];
+//             $caloPrice = NumberFormat($calOldPrice) . $gtext['currency_icon'];
+//             $total_price = NumberFormat($totalPrice) . $gtext['currency_icon'];
+//             $subtotal = NumberFormat($sub_total) . $gtext['currency_icon'];
+//             $tax = NumberFormat($totalTax) . $gtext['currency_icon'];
+//             $discount = NumberFormat($totalDiscount) . $gtext['currency_icon'];
+//             $total_amount = NumberFormat($totalAmount) . $gtext['currency_icon'];
+//         }
 
-        $old_price = '';
-        $cal_old_price = '';
-        if ($mdata['is_discount'] == 1) {
-            $old_price = '<p class="old-price">' . $oPrice . '</p>';
-            $cal_old_price = '<p class="old-price">' . $caloPrice . '</p>';
-        }
-        $transTitle = json_decode($mdata['title'], true);
-        $item_list = '<tr>
-				<td class="w-30" align="left">' . $transTitle[glan()] . '</td>
-				<td class="w-10" align="center">' . $mdata['total_room'] . '</td>
-				<td class="w-15" align="center"><p>' . $total_price . '</p>' . $old_price . '</td>
-				<td class="w-20" align="center"><p>' . date('d-m-Y', strtotime($mdata['in_date'])) . '</p><p>to</p><p>' . date('d-m-Y', strtotime($mdata['out_date'])) . '</p></td>
-				<td class="w-10" align="center">' . $total_days . '</td>
-				<td class="w-15" align="right"><p>' . $subtotal . '</p>' . $cal_old_price . '</td>
-			</tr>';
+//         $old_price = '';
+//         $cal_old_price = '';
+//         if ($mdata['is_discount'] == 1) {
+//             $old_price = '<p class="old-price">' . $oPrice . '</p>';
+//             $cal_old_price = '<p class="old-price">' . $caloPrice . '</p>';
+//         }
+//         $transTitle = json_decode($mdata['title'], true);
+//         $item_list = '<tr>
+// 				<td class="w-30" align="left">' . $transTitle[glan()] . '</td>
+// 				<td class="w-10" align="center">' . $mdata['total_room'] . '</td>
+// 				<td class="w-15" align="center"><p>' . $total_price . '</p>' . $old_price . '</td>
+// 				<td class="w-20" align="center"><p>' . date('d-m-Y', strtotime($mdata['in_date'])) . '</p><p>to</p><p>' . date('d-m-Y', strtotime($mdata['out_date'])) . '</p></td>
+// 				<td class="w-10" align="center">' . $total_days . '</td>
+// 				<td class="w-15" align="right"><p>' . $subtotal . '</p>' . $cal_old_price . '</td>
+// 			</tr>';
 
-        $RoomDataList = DB::table('room_manages')
-            ->join('room_assigns', 'room_manages.id', '=', 'room_assigns.room_id')
-            ->select('room_manages.room_no')
-            ->where('room_assigns.booking_id', $booking_id)
-            ->orderBy('room_manages.room_no', 'asc')
-            ->get();
+//         $RoomDataList = DB::table('room_manages')
+//             ->join('room_assigns', 'room_manages.id', '=', 'room_assigns.room_id')
+//             ->select('room_manages.room_no')
+//             ->where('room_assigns.booking_id', $booking_id)
+//             ->orderBy('room_manages.room_no', 'asc')
+//             ->get();
 
-        $room_no = '';
-        $f = 0;
-        foreach ($RoomDataList as $row) {
-            if ($f++) {
-                $room_no .= ', ';
-            }
+//         $room_no = '';
+//         $f = 0;
+//         foreach ($RoomDataList as $row) {
+//             if ($f++) {
+//                 $room_no .= ', ';
+//             }
 
-            $room_no .= $row->room_no;
-        }
+//             $room_no .= $row->room_no;
+//         }
 
-        $assign_rooms = '';
-        if ($room_no != '') {
-            $assign_rooms = __('Your assign  room no') . ': ' . $room_no;
-        }
+//         $assign_rooms = '';
+//         if ($room_no != '') {
+//             $assign_rooms = __('Your assign  room no') . ': ' . $room_no;
+//         }
 
-        $base_url = url('/');
-        $logo = public_path('media/' . $gtext['front_logo']);
-// #################
+//         $base_url = url('/');
+//         $logo = public_path('media/' . $gtext['front_logo']);
+// // #################
 
-        $customerName = $mdata['customer_name'];
-        $customerAddress = $mdata['customer_address'];
-        $customerCity = $mdata['city'];
-        $state = $mdata['state'];
-        $zip_code = $mdata['zip_code'];
-        $country = $mdata['country'];
-        $customer_email = $mdata['customer_email'];
-        $customer_phone = $mdata['customer_phone'];
-        $logo = public_path('media/' . $gtext['front_logo']);
+//         $customerName = $mdata['customer_name'];
+//         $customerAddress = $mdata['customer_address'];
+//         $customerCity = $mdata['city'];
+//         $state = $mdata['state'];
+//         $zip_code = $mdata['zip_code'];
+//         $country = $mdata['country'];
+//         $customer_email = $mdata['customer_email'];
+//         $customer_phone = $mdata['customer_phone'];
+//         $logo = public_path('media/' . $gtext['front_logo']);
 
-        $html = view('frontend.invoice-pdf',
-            compact('customerName', 'customerAddress', 'customerCity',
-                'state', 'zip_code', 'country',
-                'customer_email', 'customer_phone', 'logo')
-        )->toArabicHTML();
+//         $html = view('frontend.invoice-pdf',
+//             compact('customerName', 'customerAddress', 'customerCity',
+//                 'state', 'zip_code', 'country',
+//                 'customer_email', 'customer_phone', 'logo')
+//         )->toArabicHTML();
 
-        $pdf = app()->make('dompdf.wrapper');
+//         $pdf = app()->make('dompdf.wrapper');
 
-        $pdf->loadHTML($html);
+//         $pdf->loadHTML($html);
 
-        $output = $pdf->output();
+//         $output = $pdf->output();
 
-        $headers = array(
-            "Content-type" => "application/pdf",
-        );
+//         $headers = array(
+//             "Content-type" => "application/pdf",
+//         );
 
-        return response()->streamDownload(
-            fn() => print($output), // add the content to the stream
-            'invoice-' . $mdata['booking_no'] . time() . ".pdf", // the name of the file/stream
-            $headers
-        );
+//         return response()->streamDownload(
+//             fn() => print($output), // add the content to the stream
+//             'invoice-' . $mdata['booking_no'] . time() . ".pdf", // the name of the file/stream
+//             $headers
+//         );
 
-    }
+//     }
 }
