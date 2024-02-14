e7a4a5e84a942db2928d9e1f6c96de282c19044b
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2024-02-14 13:15:39 +0200

    Reservation Policy Crud

---
+++ database/migrations/2024_02_14_091727_create_reservation_policies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation_policies', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->longText('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation_policies');
+    }
+};
