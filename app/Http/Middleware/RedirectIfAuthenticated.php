8bbdf736ff6543f348cc02af5fdb15e47e5d311e
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2024-05-21 11:27:04 +0300

    register problem

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -24,6 +24,8 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
+            }else{
+                return redirect(RouteServiceProvider::Register);
             }
         }
 
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,6 +20,8 @@ class RouteServiceProvider extends ServiceProvider
     // public const HOME = '/home';
     public const HOME = '/backend/dashboard';
 
+    public const Register = '/user/register';
+
     /**
      * The controller namespace for the application.
      *
